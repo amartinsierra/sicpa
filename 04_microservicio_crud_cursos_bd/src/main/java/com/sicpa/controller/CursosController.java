d08602a8213431e4bcb59b9d9c432e613466831b
Antonio Martín <ajms66@gmail.com>
2022-05-24 12:54:35 +0200

    ejercicios 5 al 7

--- 04_microservicio_crud_cursos_bd/src/main/java/com/sicpa/controller/CursosController.java
+++ 04_microservicio_crud_cursos_bd/src/main/java/com/sicpa/controller/CursosController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sicpa.model.Curso;
 import com.sicpa.service.CursosServiceImpl;
 
+@CrossOrigin(value = "*")
 @RestController
 public class CursosController {
 	@Autowired

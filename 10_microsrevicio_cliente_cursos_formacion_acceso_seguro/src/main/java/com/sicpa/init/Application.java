277a0f5d2498eee36e01e8652ed28b25e9e46a9e
Antonio Martín <ajms66@gmail.com>
2022-05-26 09:30:41 +0200

    commit 25-5-2022

--- 10_microsrevicio_cliente_cursos_formacion_acceso_seguro/src/main/java/com/sicpa/init/Application.java
+++ 10_microsrevicio_cliente_cursos_formacion_acceso_seguro/src/main/java/com/sicpa/init/Application.java
@@ -3,6 +3,7 @@ package com.sicpa.init;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = {"com.sicpa.controller","com.sicpa.service"})
@@ -11,9 +12,14 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
 	@Bean
 	public RestTemplate template() {
-		return new RestTemplate();
+		//return new RestTemplate();
+		BasicAuthenticationInterceptor iterceptor=new BasicAuthenticationInterceptor("admin", "admin");
+		RestTemplate template=new RestTemplate();
+		template.getInterceptors().add(iterceptor);
+		return template;
 	}
+
 }

7e1b3ac57613d067be831b0c070736ca861c95a8
Antonio Martín <ajms66@gmail.com>
2022-05-26 12:44:31 +0200

    proyectos 18 y 19

--- 17_pedidos/src/main/java/com/sicpa/init/Application.java
+++ 17_pedidos/src/main/java/com/sicpa/init/Application.java
@@ -3,7 +3,10 @@ package com.sicpa.init;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 
 @EntityScan(basePackages = "com.sicpa.model")
@@ -13,6 +16,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate template() {
+		return new RestTemplate();
 	}
 
 }
